572ed78c051a6d8654a28da42961a6632692d273
sumerc <sumerc@localhost>
2011-02-07 14:38:17 +0200

    add sizes.c

---
+++ scripts/sizes.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+int 
+main(void)
+{
+    printf("size of pointer:%u\r\n", sizeof(void *));
+    printf("size of int:%u\r\n", sizeof(int));
+    printf("size of long:%u\r\n", sizeof(long));
+    printf("size of long int:%u\r\n", sizeof(long int));
+    printf("size of long long:%u\r\n", sizeof(long long));
+    printf("size of long long int:%u\r\n", sizeof(long long int));
+    printf("size of uint32_t:%u\r\n", sizeof(uint32_t));
+    printf("size of uint64_t:%u\r\n", sizeof(uint64_t));
+}
